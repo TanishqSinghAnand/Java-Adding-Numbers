157d0f807ee57518d2b7892f9db8971592274974
Tanishq0504 <codertanihsq@gmail.com>
2020-11-26 00:35:19 +0530

    Adding 2 Numbers

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print(" 5 + 8 + 10 = ");
+        int num1 = 5;
+        int num2 = 8;
+        int num3 = 10;
+        System.out.println(num1 + num2 + num3);
+    }
+}
